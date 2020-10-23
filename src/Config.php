4f2f8c261c37411f7d8c11164f7ac5bc7b8c2a13
Adam Binnersley <abinnersley@gmail.com>
2020-10-23 12:22:10 +0100

    Use short tags for arrays

--- src/Config.php
+++ src/Config.php
@@ -4,29 +4,30 @@ namespace Configuration;
 
 use DBAL\Database;
 
-class Config {
+class Config
+{
     protected $db;
-    protected $config = array();
+    protected $config = [];
     protected $table_config = 'config';
     
     /**
      * Constructor
      * @param Database $db This should be an instance of the database class
-     * @param string $config_table If the config table is different from the default set this here 
+     * @param string $config_table If the config table is different from the default set this here
      */
-    public function __construct(Database $db, $config_table = 'config') {
+    public function __construct(Database $db, $config_table = 'config')
+    {
         $this->db = $db;
         if (func_num_args() > 1) {
             $this->table_config = $config_table;
         }
         $settings = $this->db->selectAll($this->table_config);
-        if(is_array($settings)){
-            foreach($settings as $item){
+        if (is_array($settings)) {
+            foreach ($settings as $item) {
                 $this->config[$item['setting']] = $item['value'];
             }
-        }
-        else{
-            die('Please make sure the config setting have been added to the database');
+        } else {
+            echo('Please make sure the config setting have been added to the database');
         }
     }
     
@@ -35,7 +36,8 @@ class Config {
      * @param string $setting This should be the name of the setting that you wish to retrieve
      * @return mixed The setting value if it exists will be returned
      */
-    public function __get($setting) {
+    public function __get($setting)
+    {
         return $this->config[$setting];
     }
     
@@ -43,7 +45,8 @@ class Config {
      * Return all of the settings
      * @return array All of the settings will be returned
      */
-    public function getAll(){
+    public function getAll()
+    {
         return $this->config;
     }
     
@@ -53,7 +56,8 @@ class Config {
      * @param mixed $value The new value for the setting should be added here
      * @return $this
      */
-    public function __set($setting, $value) {
+    public function __set($setting, $value)
+    {
         return $this->set($setting, $value);
     }
     /**
@@ -62,7 +66,8 @@ class Config {
      * @param mixed $value The new value for the setting should be added here
      * @return $this
      */
-    public function set($setting, $value){
+    public function set($setting, $value)
+    {
         $this->config[$setting] = $value;
         return $this;
     }
@@ -73,8 +78,9 @@ class Config {
      * @param mixed $value The new value for the setting should be added here
      * @return $this
      */
-    public function update($setting, $value){
-        if($this->db->update($this->table_config, array('value' => $value), array('setting' => $setting))){
+    public function update($setting, $value)
+    {
+        if ($this->db->update($this->table_config, ['value' => $value], ['setting' => $setting], 1)) {
             $this->set($setting, $value);
         }
         return $this;
